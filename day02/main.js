29e45930fdc62cbc60e47af98b16bfc8e0670e48
Felipe Passos Ferreira <felipepassos.ti@outlook.com>
2024-10-29 16:27:30 -0300

    day02 - intrudução a funções

--- day02/main.js
+++ day02/main.js
@@ -76,3 +76,55 @@ switch(profession) {
         console.log('Sua camisa será BRANCA');
         break;
 }
+
+// DECLARANDO FUNÇÕES
+function gravidade() {
+    console.log('A gravidade da terra é:')
+    console.log(9.8)
+}
+
+// FUNÇÃO COM PARAMETROS
+function nomeCompleto(nome, sobrenome){
+    console.log(`${nome} ${sobrenome}`);
+}
+
+nomeCompleto("Felipe", "Passos");
+
+// FUNÇÃO COM RETORNO
+function somar(n1, n2) {
+    let resultado = n1 + n2;
+    return resultado;
+}
+
+let valorSomado = somar(10, 20);
+console.log(valorSomado);
+
+// FUNÇÃO COM RETORNO CONDICIONAL
+function maiorDeIdade(idade) {
+    if (idade >= 18) {
+        return true;
+    }else {
+        return false;
+    }
+}
+let age2 = 20;
+let verificacao = maiorDeIdade(age2);
+
+if (verificacao) {
+    console.log('E maior de idade.')
+} else {
+    console.log('Não é maior de idade.')
+}
+
+// EXERCÍCIO FUNÇÕES
+// Calcule a porcentagem entre dois números
+function calcPct( x1, x2) {
+    result = (x2 / x1) * 100;
+    return result;
+}
+
+let n1 = 30;
+let n2 = 8;
+let pct = calcPct(n1, n2);
+
+console.log(`${pct}% de ${n1} é ${n2} `);
