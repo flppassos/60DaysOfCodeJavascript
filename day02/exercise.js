08e839859190572f754093704b6d88c19ddfec47
Felipe Passos Ferreira <felipepassos.ti@outlook.com>
2024-10-31 11:48:16 -0300

    day02 - exercícios

---
+++ day02/exercise.js
@@ -0,0 +1,29 @@
+// EXERCÍCIOS 
+// Substitua o @ pela condicional correta para que o resultado seja TRUE
+// console.log(x @ y);
+let x = 10;
+let y = 5;
+console.log(x > y);
+
+// Substitua o @ pela condicional correta para que o resultado seja FALSE
+// console.log(w @ z);
+let w = "10"
+let z = 10;
+console.log(w === z);
+
+// Substitua o @ pela condicional correta para que o resultado seja TRUE
+// console.log(a @ b);
+let a = 10;
+let b = 5;
+console.log(a > b);
+
+// Crie uma condicional para verificar se o preço da carne está barato ou caro. 
+// PS: Até 45 está barato.
+let preco = 45.3;
+
+if (preco <= 45) {
+    console.log('O preço da carne está barato!');
+} else {
+    console.log('o preço da carne está caro.');
+}
+
