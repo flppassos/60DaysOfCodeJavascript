8a9acb9870bdac9a683069f088a0142fef99bd70
Felipe Passos <felipepassos08@gmail.com>
2025-02-10 23:38:45 -0300

    day10 - exercícios array

---
+++ day10/main.js
@@ -0,0 +1,8 @@
+/* Exercícios */
+
+// 1. Um array esparso é um tipo de array no qual a maioria dos elementos tem valores indefinidos. Crie um array esparso e atribua valores a alguns elementos do array (por exemplo, defina valores nos índices 1, 3 e 7). Imprima no console o array e também a informação de comprimento do array.
+ 
+const arrayEsparso = [,'Maria',,'Alice',,,,'Muniz'];
+console.log(arrayEsparso);
+console.log('Array de tamanho', arrayEsparso.length);
+
